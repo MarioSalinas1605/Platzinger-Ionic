4477c43b0887aba56a47b54ad334d3563f440d57
MarioSalinas <mario_1605@live.com.mx>
2018-11-21 08:21:52 -0600

    Style of Home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { User } from '../../interfaces/user';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  query: string = ''
+  users: User[]
   constructor(public navCtrl: NavController) {
 
   }
